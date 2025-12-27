ace4543a47f9cda190e805e99df24791548f4d69
yan <yhn_9455@qq.com>
2025-12-27 18:32:21 +0800

    update 2025_1227_1831

--- services/vm-service/backend/src/main/java/com/fit2cloud/service/impl/VmDefaultService.java
+++ services/vm-service/backend/src/main/java/com/fit2cloud/service/impl/VmDefaultService.java
@@ -503,7 +503,7 @@ public class VmDefaultService extends ServiceImpl<VmDefaultConfigMapper, Default
                 liveUser.setSendIp(currentUserInfo.getRemoteIp());
                 liveUser.setSendLocation(currentUserInfo.getLocation());
                 VmCloudServer vmCloudServer = validtimeMapper.selectVmCloudServerByUserId(item.getPullGroups());
-                if (vmCloudServer.getInstanceName() == null) {
+                if (vmCloudServer == null || vmCloudServer.getInstanceName() == null) {
                     liveUser.setReceiveServer("-");
                     liveUser.setReceiveStatus("-");
                     liveUser.setReceiveIp("-");
